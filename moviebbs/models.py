d61731d1ee97ee5b23da622a56ab3227fbab4886
crucis-onishi <onishi@denno-sekai.com>
2023-03-29 15:09:21 +0900

    rename category_name to name

--- moviebbs/models.py
+++ moviebbs/models.py
@@ -3,10 +3,10 @@ from django.urls import reverse
 from django.contrib.auth.models import User
 
 class Category(models.Model):
-    category_name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
 
     def __str__(self):
-        return self.category_name
+        return self.name
 
 class Article(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
