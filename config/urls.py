c04ac20a727ce8d3c93ae42ba14aa60095d93573
crucis-onishi <onishi@denno-sekai.com>
2023-04-28 18:16:14 +0900

    change toppage url

--- config/urls.py
+++ config/urls.py
@@ -1,11 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import RedirectView
-from django.views.generic import TemplateView   # 追加
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('moviebbs/', include('moviebbs.urls')),
     path('admin/', admin.site.urls),
-    path('',  RedirectView.as_view(url='/moviebbs/')),
-    path('accounts/', include('allauth.urls')), #追加
+    path('accounts/', include('allauth.urls')),
+    path('',  include('moviebbs.urls')),
 ]
