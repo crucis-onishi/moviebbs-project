15ef33e8bc7111a438ce84403ec933a71ac1f398
crucis-onishi <onishi@denno-sekai.com>
2023-05-16 09:16:39 +0900

    urlパターンの順序を最適化

--- moviebbs/urls.py
+++ moviebbs/urls.py
@@ -13,17 +13,16 @@ sitemaps = {
 }
 
 urlpatterns = [
-    # path('test/', views.sample_view, name='sample_view'),
     path('', views.IndexView.as_view(), name='index'),
-    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     path('create/', views.CreateView.as_view(), name='create'),
-    path('<slug:parent_category_slug>/<slug:category_slug>/', views.CategoryView.as_view(), name='category'),
-    path('<slug:parent_category_slug>/', views.ParentView.as_view(), name='parent_category'),
-    path('api/category/get/', views.ajax_get_category, name='ajax_get_category'),
-    path('comment/<int:pk>/', views.CommentView.as_view(), name='comment'),
-    path('<int:pk>/delete/', views.DeleteView.as_view(), name='delete'),
-    path('youtube_search/', views.YoutubeSearchView.as_view(), name='youtube_search'),
     path('dear_creator/', TemplateView.as_view(template_name="moviebbs/dear_creator.html"), name='dear_creator'),
     path('google5173443084b9a048.html/', views.google_search_console, name='google_search_console'),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='sitemap'),
+    path('youtube_search/', views.YoutubeSearchView.as_view(), name='youtube_search'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('comment/<int:pk>/', views.CommentView.as_view(), name='comment'),
+    path('<int:pk>/delete/', views.DeleteView.as_view(), name='delete'),
+    path('api/category/get/', views.ajax_get_category, name='ajax_get_category'),
+    path('<slug:parent_category_slug>/', views.ParentView.as_view(), name='parent_category'),
+    path('<slug:parent_category_slug>/<slug:category_slug>/', views.CategoryView.as_view(), name='category'),
 ]
